e4d3ddb16d3f0c50a06effd47edda558404f8504
Rafael Henrique <rafaelrocha2500@gmail.com>
2022-07-11 19:21:38 -0300

    fixed acessibility issues and check for user submitting

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const submitBtn = document.querySelector(".submit-button");
-const ratingSection = document.querySelector(".rating-section");
-const thankyouSection = document.querySelector(".thankyou-section");
-
-// Hiddes ratingSection after hit submit btn
-submitBtn.addEventListener("click", () => {
-  ratingSection.classList.add("hidden");
-});
-
-// Shows thankyouSection after hit submit btn
-submitBtn.addEventListener("click", () => {
-  thankyouSection.classList.remove("hidden");
-});
-
 const buttons = document.querySelectorAll("button");
 const voteResult = document.querySelector(".vote-result");
 const newParagraph = document.createElement("p");
@@ -21,5 +7,26 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     voteResult.appendChild(newParagraph);
     newParagraph.innerHTML = `You selected ${button.value} out of 5`;
+
+    hideRatingSection();
+    showThankyouSection();
   });
 });
+
+const submitBtn = document.querySelector(".submit-button");
+const ratingSection = document.querySelector(".rating-section");
+const thankyouSection = document.querySelector(".thankyou-section");
+
+// Hiddes ratingSection after hit submit btn
+function hideRatingSection() {
+  submitBtn.addEventListener("click", () => {
+    ratingSection.classList.add("hidden");
+  });
+}
+
+// Shows thankyouSection after hit submit btn
+function showThankyouSection() {
+  submitBtn.addEventListener("click", () => {
+    thankyouSection.classList.remove("hidden");
+  });
+}
